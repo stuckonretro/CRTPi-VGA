7c1af6963a81993e2fb20bc2db615fd0ae53b951
crtpi <64794262+crtpi@users.noreply.github.com>
2020-07-19 08:35:02 -0700

    Added v3.1 Scripts to CRTPi-VGA

--- VGA-to_opt/retropie/configs/all/runcommand-onend.sh
+++ VGA-to_opt/retropie/configs/all/runcommand-onend.sh
@@ -1,6 +1,8 @@
-# when exiting an emulator -- revert to 640x480@65hz VGA
+## when exiting an emulator -- revert to 640x480@65
 vcgencmd hdmi_timings 640 1 56 56 80 480 0 1 3 25 0 0 0 65 0 36000000 1  > /dev/null #VGA666 640x480p@65hz Timing
 tvservice -e "DMT 87" > /dev/null
 sleep 1 > /dev/
-fbset -depth 8 && fbset -depth 16 -xres 640 -yres 480 > /dev/null
+fbset -depth 8 && fbset -depth 16 && fbset -depth 24 -xres 640 -yres 480 > /dev/null #24b Depth
 tvservice -s > /dev/null
+
+###
